b76b666b8d24b3bac2d3d6e5350d48b9473a8ba1
Deacon2099 <k3514n@gmail.com>
2024-06-12 16:10:31 +0200

    Adding database migration

--- Models/CalculatorContext.cs
+++ Models/CalculatorContext.cs
@@ -4,7 +4,7 @@ namespace ProductionProcessCompanion.Models
 {
     public class CalculatorContext : DbContext
     {
-        public CalculatorContext() : base("name=ConnectionString") { }
+        public CalculatorContext() : base("MyConnectionString") { }
 
         public DbSet<SearchHistory> SearchHistory { get; set; }
         public DbSet<Module> Module { get; set; }
